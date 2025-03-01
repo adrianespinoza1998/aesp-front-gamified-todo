06433d1a31223480cec1b8855b4c3be1eb77ccbe
Adrian Espinoza Arevalo <adrianespinozaarevalo@gmail.com>
2025-03-01 20:04:27 -0300

    Añadir componente Name y CustomInput, y actualizar el estado inicial en nameSlice

---
+++ src/app/character/components/Name.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { CustomInput } from "@/components";
+import { useAppDispatch, useAppSelector } from "@/libs/redux/hooks";
+import { setName } from "@/libs/redux/slices/nameSlice";
+import React from "react";
+
+export const Name = () => {
+  const { name } = useAppSelector((state) => state.name);
+  const dispatch = useAppDispatch();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log({ name });
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setName(e.target.value));
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="space-y-5">
+      <CustomInput
+        id="character_name"
+        label="Character Name"
+        placeholder="Enter character name"
+        value={name}
+        onChange={handleChange}
+      />
+      <button
+        type="submit"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
---
+++ src/app/character/page.tsx
@@ -0,0 +1,10 @@
+import { Name } from "./components/Name";
+
+export default function CharacterPage() {
+  return (
+    <div>
+      <h1>Character</h1>
+      <Name />
+    </div>
+  );
+}
