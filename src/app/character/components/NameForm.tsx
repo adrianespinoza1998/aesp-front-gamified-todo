bb9eefd33f5e50caccc6e8dc723b9c1c3dad86f8
Adrian Espinoza Arevalo <adrianespinozaarevalo@gmail.com>
2025-03-01 20:15:53 -0300

    Añadir componente CustomButton y reemplazar botón en NameForm

--- src/app/character/components/NameForm.tsx
+++ src/app/character/components/NameForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { CustomInput } from "@/components";
+import { CustomButton, CustomInput } from "@/components";
 import { useAppDispatch, useAppSelector } from "@/libs/redux/hooks";
 import { setName } from "@/libs/redux/slices/nameSlice";
 import { useRouter } from "next/navigation";
@@ -30,12 +30,7 @@ export const NameForm = () => {
         value={name}
         onChange={handleChange}
       />
-      <button
-        type="submit"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Submit
-      </button>
+      <CustomButton type="submit" label="Submit" />
     </form>
   );
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export { CustomInput } from "./CustomInput";
+export { CustomButton } from "./CustomButton";
